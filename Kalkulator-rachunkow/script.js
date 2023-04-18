1b1c9ddcc0c6e647c4d893c7c62b8c95eb0cd7f7
Kacper <kacpercybulski@icloud.com>
2023-04-18 13:51:37 +0200

    app is finished, fixed bug that tip wasnt included in calculation

--- Kalkulator-rachunkow/script.js
+++ Kalkulator-rachunkow/script.js
@@ -7,7 +7,7 @@ const costInfo = document.querySelector('.cost-info')
 const cost = document.querySelector('.cost')
 
 const showBill = () => {
-	if (people.value === '' || price.value === '' || tip.value === '') {
+	if (people.value == '' || price.value == '' || tip.value == 0) {
 		error.textContent = 'Uzupełnij wszystkie pola!'
 	} else {
 		countBill()
@@ -15,22 +15,17 @@ const showBill = () => {
 }
 
 const countBill = () => {
-	const priceInfo = parseInt(price.value)
-	const tipInfo = parseInt(tip.value)
+	const priceInfo = parseFloat(price.value)
 	const personInfo = parseInt(people.value)
+	const tipInfo = parseFloat(tip.value)
 
-	const sum = (priceInfo + priceInfo * tipInfo) / personInfo
-    costInfo.style.display = 'block '
-
-	cost.textContent = sum.toFixed(2)
+	if (isNaN(priceInfo) || isNaN(tipInfo) || isNaN(personInfo)) {
+		error.textContent = 'Wprowadź poprawne dane w polach!'
+	} else {
+		const sum = (priceInfo + priceInfo * tipInfo) / personInfo
+		costInfo.style.display = 'block'
+		cost.textContent = sum.toFixed(2)
+	}
 }
 
-count.addEventListener('click', showBill)
-
-// 2. Stwórz funkcję, będzie sprawdzała, czy wszystkie pola zostały uzupełnione. Jeśli nie, pokaż błąd. Jeśli tak, wywołaj kolejną funkcję, która zajmie się obliczaniem rachunku.
-
-// 3. Funkcja obliczająca rachunek musi przechwycić dane z input i select oraz wykonać działanie wg wzoru:
-
-// 4.(kwota do zapłaty + (kwota do zapłaty * napiwek)) / ilość osób
-
-// 5.  Wynik działania wypisz w odpowiednim paragrafie. Nie zapomnij zaokrąglić kwoty do dwóch miejsc po przecinku!
+count.addEventListener('click', countBill)
