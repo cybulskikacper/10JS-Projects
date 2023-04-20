dce50aa7b704ab9099e8381bad5cfc642b9ccd5e
Kacper <kacpercybulski@icloud.com>
2023-04-20 21:20:54 +0200

    added array with text and created a function will do more tomorrow

--- 8Ball/script.js
+++ 8Ball/script.js
@@ -2,3 +2,13 @@ const img = document.querySelector('img')
 const input = document.querySelector('input')
 const answer = document.querySelector('.answer')
 const error = document.querySelector('.error')
+
+const answers = ['Tak!', 'Nie.', 'Może', 'Ciężko powiedzieć..', 'Nie chcesz znać odpowiedzi na to pytanie... :/']
+
+const checkInput = () => {
+	if (input.value === '') {
+		error.textContent = 'Musisz zadać jakieś pytanie!'
+	}
+}
+
+input.addEventListener('keyup', checkInput)
