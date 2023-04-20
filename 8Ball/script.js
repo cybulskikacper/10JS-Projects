df3dcffd1af2d701e853b60ce468652b87c48e7d
Kacper <kacpercybulski@icloud.com>
2023-04-20 20:43:39 +0200

    downloaded all items needed by qs

---
+++ 8Ball/script.js
@@ -0,0 +1,4 @@
+const img = document.querySelector('img')
+const input = document.querySelector('input')
+const answer = document.querySelector('.answer')
+const error = document.querySelector('.error')
