dbe1f7130987d0203f39c12783ea6b47a3c9da82
Kacper <kacpercybulski@icloud.com>
2023-04-21 17:43:55 +0200

    added animation on click

--- 8Ball/script.js
+++ 8Ball/script.js
@@ -1,14 +1,33 @@
-const img = document.querySelector('img')
+const ball = document.querySelector('img')
 const input = document.querySelector('input')
 const answer = document.querySelector('.answer')
 const error = document.querySelector('.error')
 
 const answers = ['Tak!', 'Nie.', 'Może', 'Ciężko powiedzieć..', 'Nie chcesz znać odpowiedzi na to pytanie... :/']
 
+const shakeBall = () => {
+	ball.classList.add('shake-animation')
+
+	setTimeout(checkInput, 1000)
+}
+
 const checkInput = () => {
-	if (input.value === '') {
+	if (input.value !== '' && input.value.endsWith('?')) {
+		generateAnswer()
+		error.textContent = ''
+		ball.classList.remove('shake-animation')
+	} else if (input.value !== '' && !input.value.endsWith('?')) {
+		error.textContent = 'Pytanie musi być zakończone znakiem "?".'
+		answer.textContent = ''
+	} else {
+		answer.textContent = ''
 		error.textContent = 'Musisz zadać jakieś pytanie!'
 	}
 }
 
-input.addEventListener('keyup', checkInput)
+const generateAnswer = () => {
+	const number = Math.floor(Math.random() * 5)
+	answer.innerHTML = `<span>Odpowiedź:</span> ${answers[number]}`
+}
+
+ball.addEventListener('click', shakeBall)
