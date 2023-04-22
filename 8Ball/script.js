966d6fc3c2e54706d726ad7b46da84f38e464bba
Kacper <kacpercybulski@icloud.com>
2023-04-22 12:41:23 +0200

    answers are generated on click

--- 8Ball/script.js
+++ 8Ball/script.js
@@ -3,31 +3,14 @@ const input = document.querySelector('input')
 const answer = document.querySelector('.answer')
 const error = document.querySelector('.error')
 
-const answers = ['Tak!', 'Nie.', 'Może', 'Ciężko powiedzieć..', 'Nie chcesz znać odpowiedzi na to pytanie... :/']
+const answers = ['Tak!', 'Nie.', 'Ciężko powiedzieć.', 'Nie chcesz znać odpowiedzi na to pytanie...:/']
 
-const shakeBall = () => {
-	ball.classList.add('shake-animation')
-
-	setTimeout(checkInput, 1000)
-}
-
-const checkInput = () => {
-	if (input.value !== '' && input.value.endsWith('?')) {
-		generateAnswer()
-		error.textContent = ''
-		ball.classList.remove('shake-animation')
-	} else if (input.value !== '' && !input.value.endsWith('?')) {
-		error.textContent = 'Pytanie musi być zakończone znakiem "?".'
-		answer.textContent = ''
-	} else {
-		answer.textContent = ''
-		error.textContent = 'Musisz zadać jakieś pytanie!'
-	}
-}
+console.log(answers)
 
 const generateAnswer = () => {
-	const number = Math.floor(Math.random() * 5)
-	answer.innerHTML = `<span>Odpowiedź:</span> ${answers[number]}`
+	const randomIndex = Math.floor(Math.random() * answers.length)
+	const generatedAnswer = answers[randomIndex]
+	answer.textContent = generatedAnswer // Ustawienie tekstu na wygenerowaną odpowiedź
 }
 
-ball.addEventListener('click', shakeBall)
+ball.addEventListener('click', generateAnswer)
