19d286e250a926f500eb6aca1fc6b148884f9c4f
Kacper <kacpercybulski@icloud.com>
2023-04-22 23:46:39 +0200

    clicking on start displays time in the module

---
+++ Stoper/script.js
@@ -0,0 +1,36 @@
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const stopBtn = document.querySelector('.stop')
+const resetBtn = document.querySelector('.reset')
+const historyBtn = document.querySelector('.history')
+const stopWatch = document.querySelector('.stopwatch')
+
+const time = document.querySelector('.time')
+const timeList = document.querySelector('.time-list')
+
+const modalShadow = document.querySelector('.modal-shadow')
+const closeBtn = document.querySelector('.close')
+
+let countTime = 0
+let minutes = 0
+let seconds = 0
+let isRunning = false
+
+const handleStart = () => {
+	if (!isRunning) {
+		isRunning = true
+		setInterval(() => {
+			seconds++
+			if (seconds >= 60) {
+				seconds = 0
+				minutes++
+			}
+			if (minutes >= 60) {
+				minutes = 0
+			}
+			stopWatch.textContent = `${minutes < 10 ? +minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+		}, 1000)
+	}
+}
+
+startBtn.addEventListener('click', handleStart)
