ad96181b71a212475f74587d9e5a9c304a9d196f
Kacper <kacpercybulski@icloud.com>
2023-04-22 13:18:05 +0200

    checking if input is filled or not

--- 8Ball/script.js
+++ 8Ball/script.js
@@ -5,12 +5,21 @@ const error = document.querySelector('.error')
 
 const answers = ['Tak!', 'Nie.', 'Ciężko powiedzieć.', 'Nie chcesz znać odpowiedzi na to pytanie...:/']
 
-console.log(answers)
+const checkInput = () => {
+	if (input.value !== '' && input.value.endsWith('?')) {
+		generateAnswer()
+		error.textContent = ''
+	} else if (!input.value == '' && !input.value.endsWith('?')) {
+		error.textContent = 'Pytanie musi być zakończone znakiem zapytania.'
+	} else {
+		error.textContent = 'Zadaj pytanie!'
+	}
+}
 
 const generateAnswer = () => {
 	const randomIndex = Math.floor(Math.random() * answers.length)
 	const generatedAnswer = answers[randomIndex]
-	answer.textContent = generatedAnswer // Ustawienie tekstu na wygenerowaną odpowiedź
+	answer.textContent = generatedAnswer
 }
 
-ball.addEventListener('click', generateAnswer)
+ball.addEventListener('click', checkInput)
