58436846a72a84b9c8a896ab8331ad3f89c4cd1a
Kacper <kacpercybulski@icloud.com>
2023-05-03 13:37:52 +0200

    cleaning btn and checking if input is filled or not

--- Walidator/script.js
+++ Walidator/script.js
@@ -7,15 +7,16 @@ const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
 const showError = (input, msg) => {
+	const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.error-text')
 
+	formBox.classList.add('error')
+	errorMsg.textContent = msg
+}
 
-    const formBox = input.parentElement
-    const errorMsg = formBox.querySelector('.error-text')
-
-    formBox.classList.add('error')
-    errorMsg.textContent = msg
-
-
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error')
 }
 
 const checkForm = input => {
@@ -23,12 +24,13 @@ const checkForm = input => {
 		if (el.value === '') {
 			showError(el, el.placeholder)
 		} else {
-			console.log('ok')
+			clearError(el)
 		}
 	})
 }
 
 sendBtn.addEventListener('click', e => {
+	e.preventDefault()
 	checkForm([username, pass, pass2, email])
 })
 
