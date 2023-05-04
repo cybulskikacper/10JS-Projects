cb004e0120fc1bbdaa36dda4a18f80924446983e
Kacper <kacpercybulski@icloud.com>
2023-05-04 22:49:50 +0200

    adding notes works now

---
+++ MyNotes/script.js
@@ -0,0 +1,64 @@
+const addBtn = document.querySelector('.add')
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
+const deletdBtns = document.getElementsByClassName('delete-note')
+const deleteAllBtn = document.querySelector('.delete-all')
+
+const noteArea = document.querySelector('.note-area')
+const notePanel = document.querySelector('.note-panel')
+const category = document.querySelector('#category')
+const textArea = document.querySelector('#text')
+const error = document.querySelector('.error')
+let selectedValue
+
+let cardID = 0
+
+const openPanel = () => {
+	notePanel.style.display = 'flex'
+}
+
+const closePanel = () => {
+	notePanel.style.display = 'none'
+	error.style.visibilty = 'hidden'
+	textArea.value = ''
+	category.selectedIndex = 0
+}
+
+const addNote = () => {
+	if (textArea.value !== '' && category.options[category.selectedIndex].value !== '0') {
+		createNote()
+		error.style.visibility = 'hidden'
+	} else {
+		error.style.visibility = 'visible'
+	}
+}
+
+const createNote = () => {
+	const newNote = document.createElement('div')
+	newNote.classList.add('note')
+	newNote.setAttribute('id', cardID)
+
+	newNote.innerHTML = `<div class="note-header">
+    <h3 class="note-title">${selectedValue}</h3>
+    <button class="delete-note">
+        <i class="fas fa-times icon"></i>
+    </button>
+</div>
+<div class="note-body">
+ ${textArea.value}
+</div>`
+
+	noteArea.appendChild(newNote)
+	cardID++
+	textArea.value = ''
+	category.selectedIndex = 0
+    notePanel.style.display = 'none'
+}
+
+const selectValue = () => {
+	selectedValue = category.options[category.selectedIndex].text
+}
+
+addBtn.addEventListener('click', openPanel)
+saveBtn.addEventListener('click', addNote)
+cancelBtn.addEventListener('click', closePanel)
